f79b9ccb4bb77dfd33d64e27693b7e15a513b636
lnfiniteMonkeys <dkyri001@gold.ac.uk>
2018-01-08 01:02:41 +0000

    cleaned up code, added comments and references

--- interpolated_spheres/interpolated_spheres.js
+++ interpolated_spheres/interpolated_spheres.js
@@ -1,106 +1,74 @@
-//import regression from 'regression';
-
+//Global variables
 var image1, image2, image3;
 var resized1, resized2, resized3;
 var slider1, slider2, slider3;
 var button1, button2, button3;
 var model1, model2, model3;
-
 var dataset1 = [], dataset2 = [], dataset3 = [];
-var coefficients;
 
-var testCoeff = [1, 1, 1, 0];
+//Current stage
+var stage;
 
-var lerpedEquation;
+var pad = 10;
+var imgTop = 120;
 
-
-var stage = 0;
-
-var degreeSlider;
+//Degree of the polynomial; if you change it here remember to change it
+//in the fragment shader too (it seems that GLSL needs literal numbers for
+//loops so the degree can't be passed as a uniform)
 var polyDegree = 7;
+//Initial coefficients set to 0
 var u_coeffs = [];
 for(let i = 0; i < polyDegree; i++) u_coeffs.push(0);
 
+//The interpolated coefficients that get sent to the shader
+var lerpedCoefficients;
+
+
+
+//Dimensions of resized images
 var resizedX = 200;
 var resizedY = 200;
 
-var isReady = false;
-
-//THREEjs variables
+//THREEjs standard objects
 var container;
 var camera, scene, renderer;
 var uniforms;
 
-
+//Load all three images
 function preload(){
   loadImages();
 }
 
+//Setup screen
 function setup() {
+  stage = 0;
   setupCanvas();
+  background(255);
   initUI();
-
-  //sliders.push(createSlider(0, 1, 0.5));
-  //LoadImage with callback
 }
 
-var pct = 0;
 function draw() {
-  background(255);
-
-  if(stage == 0){
+  if(stage == 0)
     drawImages();
-  }
   else if(stage == 1){
-    drawImages();
+    //drawImages();
     drawPolynomials();
-    lerpedEquation = lerpPolynomials(model1.equation, model2.equation, pct)
-    //let newPoly = lerpPolynomials(model1.equation, model2.equation, 0.5+0.5*Math.sin(millis()/1000));
-    drawPolynomial(lerpedEquation);
   }
-
   else if(stage == 2){
+    //Interpolates the coefficients that are sent to the shader
+    //Goes from model 1 to 2, from 2 to 3 and then back again
+    lerpedCoefficients = animation();
+    //Update the uniforms sent to the shader, time and interpolated coefficients
     uniforms.time.value = millis()/1000;
-    uniforms.coeffs.value = model1.equation; //u_coeffs;
-    // uniforms.resolution.value = [window.innerWidth]
+    uniforms.u_coefficients.value = lerpedCoefficients;
     renderer.render( scene, camera );
   }
 }
 
+//Keyboard controls: SPACE to move to the next stage
 function keyPressed(){
-  if(key == '1' && stage == 0){
-    resizeImages();
-    makeDatasets();
-    makeModels();
-    console.log("Coefficients: ", model1.equation);
-    stage = 1;
-  }
-
-  if(key == ' '){
-    stage = 0;
-    loadImages();
-    console.log("state reset to 0");
-  }
-
-  if(key == '2'){
-    pct = 0;
-    console.log("pct: ", pct);
-  }
-  if(key == '3'){
-    pct = 1;
-    console.log("pct: ", pct);
-  }
-
-  if(key == '4'){
-    console.log("pct: ", pct);
-    console.log("m1: ", model1.equation);
-    console.log("m2: ", model2.equation);
-    console.log("lerped1: ", lerpPolynomials(model1.equation, model2.equation, pct));
-
-    // console.log("m1: ", model1.equation);
-    // console.log("m2: ", model2.equation);
-    // console.log("lerped0: ", lerpPolynomials(model1.equation, model2.equation, 0));
-    // console.log("lerped1: ", lerpPolynomials(model1.equation, model2.equation, 1));
-  }
-
+  if(key == ' ' && stage == 0)
+    train();
+  else if(key == ' ' && stage == 1)
+    showShader();
 }
--- interpolated_spheres/util.js
+++ interpolated_spheres/util.js
@@ -1,47 +1,150 @@
 //--------------------------------------------------------------
+function train(){
+  if(stage == 0){
+    //Downsize the thresholded images
+    resizeImages();
+    //Make the datasets based on the (normalized) x and y coordinates off each black pixel
+    makeDatasets();
+    //Train three models based on those datasets
+    makeModels();
+    stage = 1;
+    console.log("Training Done");
+  }
+}
+
+//--------------------------------------------------------------
+function makeDatasets(){
+  //Threshold for a pixel to be considered black
+  let threshold = 50;
+
+  //Loop through all the pixels and, if they're under the brightness threshold,
+  //add them to the dataset using their (normalized) x coordinates as input and their y coordinates as output
+
+  resized1.loadPixels();
+  for(let x = 0; x < resizedX; x++){
+    for(let y = 0; y < resizedY; y++){
+      let loc = (x + y * resizedX)*4;
+      if(resized1.pixels[loc] < threshold){
+        dataset1.push([x/resizedX, 1-y/resizedY]);
+      }
+    }
+  }
+
+  resized2.loadPixels();
+  for(let x = 0; x < resizedX; x++){
+    for(let y = 0; y < resizedY; y++){
+      let loc = (x + y * resizedX)*4;
+      if(resized2.pixels[loc] < threshold){
+        dataset2.push([x/resizedX, 1-y/resizedY]);
+      }
+    }
+  }
+
+  resized3.loadPixels();
+  for(let x = 0; x < resizedX; x++){
+    for(let y = 0; y < resizedY; y++){
+      let loc = (x + y * resizedX)*4;
+      if(resized3.pixels[loc] < threshold){
+        dataset3.push([x/resizedX, 1-y/resizedY]);
+      }
+    }
+  }
+}
+
+//Linearly interpolates the coefficients of the models going from 1 to 2, 2 to 3 and back around
+function animation(){
+  let morph;
+  //How long it takes to complete a cycle
+  let duration = 35*1000;
+  let timer = (millis()%duration)/duration;
+  //First half of the animation, going from model 1 to model 3
+  if(timer < 0.25){
+    morph = lerpPolynomials(model1.equation, model2.equation, map(timer, 0.0, 0.25, 0.0, 1.0));
+  }
+  else if (timer >= 0.25 && timer < 0.5){
+    morph  = lerpPolynomials(model2.equation, model3.equation, map(timer, 0.25, 0.5, 0.0, 1.0));
+  }  //Second half, coming back again
+  else if(timer >= 0.5 && timer < 0.75){
+    morph = lerpPolynomials(model3.equation, model2.equation, map(timer, 0.5, 0.75, 0.0, 1.0));
+  }
+  else if (timer >= 0.75){
+    morph  = lerpPolynomials(model2.equation, model1.equation, map(timer, 0.75, 1.0, 0.0, 1.0));
+  }
+  return morph;
+}
+
+//Trains three models based on the datasets
+//--------------------------------------------------------------
+function makeModels(){
+  model1 = regression.polynomial(dataset1, {order: polyDegree});
+  model2 = regression.polynomial(dataset2, {order: polyDegree});
+  model3 = regression.polynomial(dataset3, {order: polyDegree});
+}
+
+//--------------------------------------------------------------
+function showShader(){
+  slider1.remove();
+  slider2.remove();
+  slider3.remove();
+  button1.remove();
+  button2.remove();
+  button3.remove();
+  //Deletes the P5js canvas and reveals the THREEjs fragment shader
+  noCanvas();
+  stage = 2;
+}
+
+//Draws the three models on top of the images
+function drawPolynomials(){
+  drawPolynomial(model1.equation, 0, imgTop, width/3, width/3);
+  drawPolynomial(model2.equation, width/3, imgTop, width/3, width/3);
+  drawPolynomial(model3.equation, 2*width/3, imgTop, width/3, width/3);
+}
+
+//Draws a polynomial
+function drawPolynomial(poly, startX, startY, _width, _height){
+  let numPoints = 200;
+  let size = 8;
+
+  for(let i = 0; i < numPoints; i++){
+    let x = i/numPoints;
+    let y = 0;
+    for(let d = 0; d < poly.length; d++){
+      y += poly[d]*Math.pow(x, poly.length-d-1);
+    }
+
+    fill(255, 100, 100);
+    ellipse(startX + _width*i/numPoints, startY + _height-_height*y, size, size);
+  }
+}
+
+//Linearly interpolates between two polynomials of the same degree
+//--------------------------------------------------------------
+function lerpPolynomials(poly1, poly2, pct){
+  var newPoly = [];
+  for(let i = 0; i < polyDegree+1; i++){
+    newPoly.push(lerp(poly1[i], poly2[i], pct));
+  }
+
+  return newPoly;
+}
+//--------------------------------------------------------------
+function drawImages(){
+  image(image1, 0, imgTop, (width-2*pad)/3, width/3);
+  image(image2, (width-pad)/3, imgTop, (width-2*pad)/3, width/3);
+  image(image3, 2*(width-pad)/3, imgTop, (width-2*pad)/3, width/3);
+}
+
+
+//--------------------------------------------------------------
 function loadImages(){
   //loads 3 images, displays them on screen with three sliders to change
-  //the thresholding
-
+  //the brightness thresholds
   image1 = loadImage("data/image1.jpg");
   image2 = loadImage("data/image2.jpg");
   image3 = loadImage("data/image3.jpg");
-
-  // img = loadImage("data/handSketch.jpg", function(img) {
-  //   // img.resize(200, 200);
-  //   img.filter(THRESHOLD, 0.1);
-  //   image(img, 0, 0, width, height);
-  //   // img.resize(200, 200);
-  //   // let blackPix = [];
-  //   // let xs = [];
-  //   // let ys = [];
-  //   //
-  //   // img.loadPixels();
-  //   // for(var x = 0; x < img.width; x++){
-  //   //   for(var y = 0; y < img.height; y++){
-  //   //     let loc = x + y*width;
-  //   //     let pix = img.pixels[loc];
-  //   //     if(pix < 200){
-  //   //       blackPix.push(pix);
-  //   //       xs.push(x);
-  //   //       ys.push(y);
-  //   //     }
-  //   //   }
-  //   // }
-  //   //
-  //   // let testSet = [];
-  //   // for(var i = 0; i < blackPix.length; i++){
-  //   //   testSet.push([xs[i]/img.width, 1-ys[i]/img.height]);
-  //   // }
-  //   // // console.log(testSet);
-  //   // weki.deleteTraining();
-  //   // weki.sendSet(testSet);
-  //
-  //   // img.filter("gray");
-  //   // img.filter("threshold", 0.15);
-  //   // save(img, "myImage.jpg");
-  // });
 }
+
 //--------------------------------------------------------------
 function resizeImages(){
   resized1 = image1;
@@ -53,6 +156,7 @@ function resizeImages(){
   resized3 = image3;
   resized3.resize(resizedX, resizedY);
 }
+
 //--------------------------------------------------------------
 function filterImg1(){
   let value = slider1.value();
@@ -72,132 +176,6 @@ function filterImg3(){
       i.filter(THRESHOLD, value);
   });
 }
-//--------------------------------------------------------------
-function makeDatasets(){
-  let threshold = 50;
-  resized1.loadPixels();
-  for(let x = 0; x < resizedX; x++){
-    for(let y = 0; y < resizedY; y++){
-      let loc = (x + y * resizedX)*4;
-      if(resized1.pixels[loc] < threshold){
-        dataset1.push([x/resizedX, y/resizedY]);
-      }
-    }
-  }
-
-  console.log(dataset1);
-
-  resized2.loadPixels();
-  for(let x = 0; x < resizedX; x++){
-    for(let y = 0; y < resizedY; y++){
-      let loc = (x + y * resizedX)*4;
-      if(resized2.pixels[loc] < threshold){
-        dataset2.push([x/resizedX, y/resizedY]);
-      }
-    }
-  }
-
-  resized3.loadPixels();
-  for(let x = 0; x < resizedX; x++){
-    for(let y = 0; y < resizedY; y++){
-      let loc = (x + y * resizedX)*4;
-      if(resized3.pixels[loc] < threshold){
-        dataset3.push([x/resizedX, y/resizedY]);
-      }
-    }
-  }
-}
-//--------------------------------------------------------------
-function makeModels(){
-  model1 = regression.polynomial(dataset1, {order: polyDegree});
-  model2 = regression.polynomial(dataset2, {order: polyDegree});
-  model3 = regression.polynomial(dataset3, {order: polyDegree});
-}
-//--------------------------------------------------------------
-function showShader(){
-  noCanvas();
-
-  stage = 2;
-}
-
-function drawPolynomials(){
-  let numPoints = 200;
-  for(let i = 0; i < numPoints; i++){
-    let m1 = model1.predict(i/numPoints);
-    let m2 = model2.predict(i/numPoints);
-    let m3 = model3.predict(i/numPoints);
-    fill(255, 0, 0);
-    ellipse(width*m1[0], height*m1[1], 5, 5);
-
-    fill(0, 255, 0);
-    ellipse(width*m2[0], height*m2[1], 5, 5);
-
-    fill(0, 0, 255);
-    ellipse(width*m3[0], height*m3[1], 5, 5);
-  }
-}
-
-function drawPolynomial(poly){
-  let numPoints = 200;
-
-  for(let i = 0; i < numPoints; i++){
-    let x = i/numPoints;
-    let y = 0;
-    for(let d = 0; d < poly.length; d++){
-      y += poly[d]*Math.pow(x, poly.length-d-1);
-    }
-
-    fill(255, 100, 100);
-    ellipse(width*i/numPoints, height*y, 8, 8);
-  }
-
-}
-
-function lerpPolynomials(poly1, poly2, pct){
-  var newPoly = [];
-  for(let i = 0; i < polyDegree+1; i++){
-    newPoly.push(lerp(poly1[i], poly2[i], pct));
-  }
-
-  return newPoly;
-}
-//--------------------------------------------------------------
-function drawImages(){
-  image(image1, 0, 0, width/3, 400);
-  image(image2, width/3, 0, width/3, 400);
-  image(image3, 2*width/3, 0, width/3, 400);
-}
-
-//--------------------------------------------------------------
-function initUI(){
-  degreeSlider = createSlider(2, 20, 7, 1);
-  degreeSlider.position(10, 100);
-  degreeSlider.style('width', '80px');
-
-  slider1 = createSlider(0, 1, 0.5, 0.001);
-  slider1.position(10, 10);
-  slider1.style('width', '80px');
-
-  slider2 = createSlider(0, 1, 0.5, 0.001);
-  slider2.position(10, 30);
-  slider2.style('width', '80px');
-
-  slider3 = createSlider(0, 1, 0.5, 0.001);
-  slider3.position(10, 50);
-  slider3.style('width', '80px');
-
-  button1 = createButton('Filter Image 1');
-  button1.position(159, 19);
-  button1.mousePressed(filterImg1);
-
-  button2 = createButton('Filter Image 2');
-  button2.position(279, 19);
-  button2.mousePressed(filterImg2);
-
-  button3 = createButton('Filter Image 3');
-  button3.position(499, 19);
-  button3.mousePressed(filterImg3);
-}
 
 //--------------------------------------------------------------
 function setupCanvas(){
@@ -205,19 +183,18 @@ function setupCanvas(){
   var canvas = createCanvas(window.innerWidth, window.innerHeight );
   canvas.parent("container");
 
-  //THREEjs window setup, adapted from the shader example
+  //THREEjs window setup, adapted from the website's shader example
   container = document.getElementById( 'container' );
   camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
 	scene = new THREE.Scene();
 	var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
 
-  console.log("window: ", window.innerWidth, window.innerHeight );
   //Time and the polynomial coefficients are sent to the shader as uniforms
   uniforms = {
 					time: { value: 1.0 },
           resolution: {type: "v2",
                        value: new THREE.Vector2(1920, 1080)},
-          coeffs: {type: "fv1",
+          u_coefficients: {type: "fv1", //basic float vector of variable length
                    value: u_coeffs}
 	};
 
@@ -228,7 +205,7 @@ function setupCanvas(){
 		fragmentShader: document.getElementById( 'fragmentShader' ).textContent
 	});
 
-  //Binding the material on the plane geometry
+  //Binding the material on the full-screen plane geometry
   var mesh = new THREE.Mesh( geometry, material );
   scene.add( mesh );
   renderer = new THREE.WebGLRenderer();
@@ -236,6 +213,42 @@ function setupCanvas(){
   container.appendChild( renderer.domElement );
   onWindowResize();
   window.addEventListener( 'resize', onWindowResize, false );
+}
+
+//--------------------------------------------------------------
+function initUI(){
+  let y = imgTop + width/3 + 15;
+  let w = (width-pad);
+  let bOff = 120;
+
+  slider1 = createSlider(0, 1, 0.5, 0.001);
+  slider1.position(pad, y);
+  slider1.style('width', '80px');
+
+  slider2 = createSlider(0, 1, 0.5, 0.001);
+  slider2.position((width-pad)/3, y);
+  slider2.style('width', '80px');
+
+  slider3 = createSlider(0, 1, 0.5, 0.001);
+  slider3.position(2*(width-pad)/3, y);
+  slider3.style('width', '80px');
+
+  button1 = createButton('Filter Image 1');
+  button1.position(pad + bOff, y);
+  button1.mousePressed(filterImg1);
+
+  button2 = createButton('Filter Image 2');
+  button2.position((width-pad)/3 + bOff, y);
+  button2.mousePressed(filterImg2);
+
+  button3 = createButton('Filter Image 3');
+  button3.position(2*(width-pad)/3 + bOff, y);
+  button3.mousePressed(filterImg3);
+
+  //Instructions
+  var t = "1) Filter the images until there are no artifacts, \n2) Press Space once to see the models, \n3) Press Space again to see the shader \n4) Place 3 other images in the data folder (named image1.jpg, image2.jpg etc.) and refresh the page!";
+  textSize(20);
+  text(t, 10, 30);
 }
 
 //--------------------------------------------------------------
